2cf47ac2a2075c242abfb535ea22f0670ab96b70
SinSeungHyuk <55730186+SinSeungHyuk@users.noreply.github.com>
2024-02-07 20:45:58 +0900

    초기 커밋.

---
+++ Thread/Thread.cpp
@@ -0,0 +1,201 @@
+﻿// Thread.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+
+
+using namespace std;
+using std::thread;
+
+
+
+void func1() {
+    for (int i = 0; i < 10; i++) cout << "스레드 1 작동중\n";
+}
+void func2() {
+    for (int i = 0; i < 10; i++) cout << "스레드 2 작동중\n";
+}
+void func3() {
+    for (int i = 0; i < 10; i++) cout << "스레드 3 작동중\n";
+}
+
+void worker(vector<int>::iterator s, vector<int>::iterator end, int* result) {
+    int sum = 0;
+    for (auto itr = s; itr < end; ++itr) {
+        sum += *itr;
+    }
+    *result = sum;
+
+    // 스레드 id 구하기
+    thread::id this_id = this_thread::get_id();
+    printf("스레드 %x 에서 %d 부터 %d 까지 계산한 결과: %d\n",
+        this_id, *s, *(end - 1), sum);
+}
+
+mutex m; // mutex 객체
+void worker2(int* cnt) {
+    for (int i = 0; i < 10000; i++) {
+        lock_guard<mutex> mutex_guard(m);
+        *cnt += 1;
+    }
+}
+
+void worker3(promise<string>* p) {
+    // 약속 이행. 이 결과는 future에 들어간다.
+    this_thread::sleep_for(chrono::seconds(10)); // 10초동안 스레드 대기
+    p->set_value("promise data");
+}
+
+void runner(shared_future<void> start) {
+    start.get();
+    cout << "출발\n";
+}
+
+int task(int x) { return 10 + x; }
+
+int sum(const vector<int>& v, int start, int end) {
+    int total = 0;
+    for (int i = start; i < end; i++) {
+        total += v[i];
+    }
+    return total;
+}
+int parallel_sum(const vector<int>& v) {
+    future<int> lower_half_future =
+        async(launch::async, sum, ref(v), 0, v.size() / 2);
+    // upper : 501~1000 더하기
+    int upper_half = sum(v, v.size() / 2, v.size());
+
+    return lower_half_future.get() + upper_half;
+}
+
+// ============== semaphore
+
+counting_semaphore<10> semaphore{ 5 }; // least max value : 1, counter : 1
+
+void worker_sema(int id) {
+    semaphore.acquire(); // wait (P) 연산
+
+    // doing work
+    cout << "thread " << id << " 세마포를 성공적으로 얻어냄" << endl;
+
+    // releasing 반환
+    semaphore.release(); // signal (V) 연산
+    cout << "thread " << id << " 세마포에 반환함" << endl;
+}
+
+binary_semaphore b_semaphore(1);
+
+void worker_b_sema(int id) {
+    b_semaphore.acquire(); // wait (P) 연산
+
+    // doing work
+    cout << "thread " << id << " 이진 세마포 얻어냄" << endl;
+
+    // 반환
+    b_semaphore.release(); // signal (V) 연산
+    cout << "thread " << id << " 세마포 반환" << endl;
+}
+
+
+int main()
+{
+    //thread t1(func1);
+    //thread t2(func2);
+    //thread t3(func3);
+
+    //t1.detach();
+    //t2.detach();
+    //t3.detach();
+    //===============================================
+
+    //vector<int> data(10000);
+    //for (int i = 1; i <= 10000; i++) data[i-1] = i;
+
+    //// 각 스레드에서 계산된 부분 합들을 저장하는 벡터
+    //vector<int> partial_sums(4);
+
+    //vector<thread> workers;
+    //for (int i = 0; i < 4; i++) {
+    //    workers.push_back(thread(worker, data.begin() + (i * 2500),
+    //        data.begin() + (i+1) * 2500, &partial_sums[i]));
+    //}
+
+    //for (int i = 0; i < 4; i++)  workers[i].join();
+
+    //int total = 0;
+    //for (int i = 0; i < 4; i++) total += partial_sums[i];
+    //===============================================
+
+    //int cnt = 0;
+    //vector<thread> workers;
+    //for (int i = 0; i < 4; i++) {
+    //    workers.push_back(thread(worker2, &cnt));
+    //}
+    //for (int i = 0; i < 4; i++) {
+    //    workers[i].join();
+    //}
+        //===============================================
+
+    //promise<string> p;
+    //// 미래에 string 데이터를 돌려주는 약속
+    //future<string> data = p.get_future();
+    //thread t(worker3, &p);
+
+    //data.wait(); // 약속된 데이터 받을때까지 기다리기
+
+    //while (true) {
+    //    // 1초씩 기다려보기
+    //    future_status status = data.wait_for(chrono::seconds(1));
+
+    //    if (status == future_status::timeout) {
+    //        cerr << ">"; // cerr : cout과 달리 버퍼를 바로 비워 디버깅에 용이
+    //    }
+
+    //    else if (status == future_status::ready) {
+    //        cout << data.get();
+    //        break;
+    //    }
+    //}
+    //===============================================
+    // 
+    // 
+    //promise<void> p;
+    //shared_future<void> start = p.get_future();
+
+    //thread t1(runner, start);
+    //thread t2(runner, start);
+    //thread t3(runner, start);
+
+    //cerr << "준비..";
+    //this_thread::sleep_for(chrono::seconds(1));
+    //cerr << " 시작\n";
+
+    //p.set_value();
+    //
+    //t1.join();
+    //t2.join();
+    //t3.join();
+    //===============================================
+
+    // int task(int x) { return 10 + x; }
+    //packaged_task<int(int)> pack_task(task);
+    //future<int> start = pack_task.get_future();
+
+    //thread t(move(pack_task), 5);
+    //cout << "결과: " << start.get();
+        //===============================================
+
+   
+    //======================== 세마포
+
+    thread t1(worker_sema, 1);
+    thread t2(worker_sema, 2);
+    thread t3(worker_sema, 3);
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    cout << endl << endl;
+}
---
+++ Thread/pch.h
@@ -0,0 +1,94 @@
+﻿// pch.h: 미리 컴파일된 헤더 파일입니다.
+// 아래 나열된 파일은 한 번만 컴파일되었으며, 향후 빌드에 대한 빌드 성능을 향상합니다.
+// 코드 컴파일 및 여러 코드 검색 기능을 포함하여 IntelliSense 성능에도 영향을 미칩니다.
+// 그러나 여기에 나열된 파일은 빌드 간 업데이트되는 경우 모두 다시 컴파일됩니다.
+// 여기에 자주 업데이트할 파일을 추가하지 마세요. 그러면 성능이 저하됩니다.
+
+#ifndef PCH_H
+#define PCH_H
+
+// 여기에 미리 컴파일하려는 헤더 추가
+
+#endif //PCH_H
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+#include<thread>
+#include <cstdio>
+#include <mutex>
+#include <future>
+#include <chrono>
+#include <exception>
+#include <semaphore>
+
+
+//int func1(const string& s) {
+//    cout << "fun1 호출 :" << s << endl;
+//    return 0;
+//}
+//
+//struct Func {
+//    void operator() (string s) { cout << "func2 호출 :" << s << endl; }
+//};
+//
+//class A {
+//public:
+//    int n;
+//    A(int num) : n(num) {};
+//    int funcA() { cout << "A : " << n << endl; return n; }
+//    void funcB() { cout << "funcB 호출" << endl; }
+//};
+//void funcC(const A& a, A& b) { b.n = a.n; cout << "레퍼런스 접근 A : " << b.n << endl; }
+//
+//void sub(int n, int m) {
+//    cout << "n-m = " << n - m << endl;
+//}
+
+    //function<int(const string&)> f1 = func1;
+    //f1("함수를 객체처럼 사용");
+
+    //function<void(string)> f2 = Func();
+    //f2("연산자 오버로딩");
+
+    //function<void()> f3 = []() {cout << "람다식 func3 호출"; };
+    //f3();
+    //cout << endl;
+    //// =================================================
+    //
+    //A ca(3); // A의 객체 a 생성
+    //function<int(A&)> fA = &A::funcA;
+    //fA(ca);
+    //// =================================================
+
+    //vector<int> a(1);
+    //vector<int> b(2);
+    //vector<int> c(3);
+    //vector<int> d(4);
+
+    //vector<vector<int>> container;
+    //container.push_back(b);
+    //container.push_back(d);
+    //container.push_back(a);
+    //container.push_back(c);
+
+    //vector<int> size_vec(4);
+
+    ////function<size_t(const vector<int>&)> func_size = &vector<int>::size;
+
+    //transform(container.begin(), container.end(), size_vec.begin(),
+    //    mem_fn(&vector<int>::size));
+    //for (auto itr = size_vec.begin(); itr != size_vec.end(); ++itr) {
+    //    cout << "벡터 크기 :: " << *itr << endl;
+    //}
+    //// =================================================
+
+    //auto sub_from_3 = bind(sub, placeholders::_2 , placeholders::_1);
+    //sub_from_3(4,2); // 2 - 4
+
+    //A refA(5);
+    //A refB(7);
+    //auto ref_with_A = bind(funcC, (refA), placeholders::_1);
+    //ref_with_A(refB);
